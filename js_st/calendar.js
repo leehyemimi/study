0a50567d5a8959c156acca1cb26df64e084927bf
leehyemimi <sunone222@gmail.com>
2020-12-16 22:05:41 +0900

    Update calendar.js
    주석

--- js_st/calendar.js
+++ js_st/calendar.js
@@ -1,4 +1,4 @@
-function fnGetClassName(NowDay){ //요일 클래스
+function fnGetClassName(NowDay){
 	var className = "";
 	if(NowDay === 0){
 		className = "sun";
@@ -8,7 +8,7 @@ function fnGetClassName(NowDay){ //요일 클래스
 	}
 	return className;
 }
-function getFormatDate(date){ // 날짜포맷 yyyy.MM.dd 변환
+function getFormatDate(date){
 	var year = date.getFullYear();
 	var month = (1 + date.getMonth());
 	month = month >= 10 ? month : '0' + month;
@@ -17,58 +17,64 @@ function getFormatDate(date){ // 날짜포맷 yyyy.MM.dd 변환
 	return year + '.' + month + '.' + day;
 }
 
-//실행순서 ->
-	//1. -> 2. -> 3. -> 4. ->
-//
+/*
+// 실행순서
+1. constructor 생성자 생성
+2. create() 메서드 실행
+3. CalendarClickOpen() 메서드 실행
+4. constructor 생성자 매개변수중 url에 값이 있을경우 JsonUrl() 메서드 실행
+5. Calendar에 삭제 버튼 클릭시 CalendarClickClose() 메서드 실행
+*/
 
 class Calendar {
-	constructor(nowDate,inputId,url) { //생성자 : class 인스턴스를 생성할때 초기화 하는 메소드
-		this.inputId = inputId;
-		this.nowDate = nowDate.split('.');
-		this.nowDateY = this.nowDate[0];
-		this.nowDateM = this.nowDate[1];
-		this.nowDateD = this.nowDate[2];
-		this.today = new Date(this.nowDateY,this.nowDateM,this.nowDateD);
-		this.nowDate = getFormatDate(this.today);
+	constructor(nowDate,inputId,url) { //생성자 생성 constructor('YYYY.MM.DD', 'input 아이디' , 'Jsonurl')
+		this.inputId = inputId; //input 아이디
 
-		this.year = this.today.getFullYear(); //년도
+		this.nowDate = nowDate.split('.');
+		this.nowDateY = this.nowDate[0]; //년
+		this.nowDateM = this.nowDate[1]; //월
+		this.nowDateD = this.nowDate[2]; //일
+		this.today = new Date(this.nowDateY,this.nowDateM,this.nowDateD);
+		this.nowDate = getFormatDate(this.today);  //날짜
+
+		this.year = this.today.getFullYear(); //년
 		this.month = this.today.getMonth(); //월
 		this.nowMonth = this.today.getMonth() + 1; //월
-		this.date = this.today.getDate(); //
+		this.date = this.today.getDate(); //일
 
-		this.firstDate = new Date(this.year,this.month ,1); //첫째날
-		this.firstDateDay = this.firstDate.getDay(); //첫째날 요일
-		this.lastDate = new Date(this.year,this.month+1,0); //마지막날
-		this.lastDate = this.lastDate.getDate(); //마지막날짜
-		this.d = 1; //달력에 표시될 날짜
-		this.trLength = Math.ceil((this.lastDate + this.firstDateDay) /7); //tr갯수 시작날짜 + 총 달 날짜*!/
-		this.totalTd = this.trLength * 7;
-		this.calendarId = 'calendar' + this.inputId.slice(-1);
+		this.firstDate = new Date(this.year,this.month ,1);
+		this.firstDateDay = this.firstDate.getDay(); //시작하는요일
+		this.lastDate = new Date(this.year,this.month+1,0);
+		this.lastDate = this.lastDate.getDate(); //끝나는날짜
+		this.d = 1;
 
+		this.trLength = Math.ceil((this.lastDate + this.firstDateDay)/7); //tr갯수
+		this.totalTd = this.trLength * 7; //td갯수
+		this.calendarId = 'calendar' + this.inputId.slice(-1); //calendar id
+
+		// Json
+		this.url = url; //Json url
 		this.newContent = [];
 		this.newContentTodo = [];
 		this.newContentHoliyday = [];
-		this.url = url;
 		this.todo = "";
 
 		var _this = this;
-		_this.CalendarClickOpen();
 		_this.create(nowDate);
+		_this.CalendarClickOpen();
 	}
 
-	create(nowDate){ //달력레이어 만들기
+	create(nowDate){
 		var _this = this;
-
-		//input날짜
-		var selectNowDay = document.getElementById(_this.inputId).value,
-			selectNowDay = selectNowDay.split('.'),
+		var selectNow = document.getElementById(_this.inputId).value,
+			selectNowDay = selectNow.split('.'),
 			selectNowDayY = selectNowDay[0],
 			selectNowDayM = selectNowDay[1],
 			selectNowDayD = selectNowDay[2],
 			selectDay = new Date(selectNowDayY, selectNowDayM - 1, selectNowDayD),
-			selectYear = selectDay.getFullYear(), //년도
-			selectMonth = selectDay.getMonth() + 1, //월
-			selectDate = selectDay.getDate(); //일
+			selectYear = selectDay.getFullYear(),
+			selectMonth = selectDay.getMonth() + 1,
+			selectDate = selectDay.getDate();
 
 		_this.nowDate = nowDate;
 		_this.nowDate = _this.nowDate.split('.');
@@ -76,18 +82,20 @@ class Calendar {
 		_this.nowDateM = _this.nowDate[1];
 		_this.nowDateD = _this.nowDate[2];
 		_this.today = new Date(_this.nowDateY, _this.nowDateM - 1, _this.nowDateD);
-		_this.year = _this.today.getFullYear(); //년도
-		_this.month = _this.today.getMonth(); //월
-		_this.nowMonth = _this.today.getMonth() + 1; //월
-		_this.date = _this.today.getDate(); //일
+
+		_this.year = _this.today.getFullYear();
+		_this.month = _this.today.getMonth();
+		_this.nowMonth = _this.today.getMonth() + 1;
+		_this.date = _this.today.getDate();
 		_this.today = new Date(_this.year, _this.month, _this.date);
 
-		_this.firstDate = new Date(_this.year, _this.month, 1); //첫째날
-		_this.firstDateDay = _this.firstDate.getDay(); //첫째날 요일
-		_this.lastDate = new Date(_this.year, _this.month + 1, 0); //마지막날
-		_this.lastDate = _this.lastDate.getDate(); //마지막날짜
-		_this.d = 1;//달력에 표시될 날짜
-		_this.trLength = Math.ceil((_this.lastDate + _this.firstDateDay) / 7); //tr갯수 시작날짜 + 총 달 날짜*!/
+		_this.firstDate = new Date(_this.year, _this.month, 1);
+		_this.firstDateDay = _this.firstDate.getDay();
+		_this.lastDate = new Date(_this.year, _this.month + 1, 0);
+		_this.lastDate = _this.lastDate.getDate();
+		_this.d = 1;
+
+		_this.trLength = Math.ceil((_this.lastDate + _this.firstDateDay) / 7);
 		_this.totalTd = _this.trLength * 7;
 		_this.calendarId = 'calendar' + _this.inputId.slice(-1);
 
@@ -107,31 +115,30 @@ class Calendar {
 
 			if (j >= _this.firstDateDay && _this.d <= _this.lastDate) {
 				var day_date = new Date(_this.year, _this.month, _this.d),
-					nowDay = day_date.getDay();//요일
-				var _nowDate = getFormatDate(day_date),
+					nowDay = day_date.getDay(),
+					 _nowDate = getFormatDate(day_date),
 					className = "",
 					openTr = "",
 					closeTr = "";
 
-				//요일 클래스
-				className = fnGetClassName(nowDay);
+				className = fnGetClassName(nowDay); //class
 
-				//현재 날짜 클래스
-				if (selectYear === _this.year && selectMonth === _this.nowMonth) {
+
+				if (selectYear === _this.year && selectMonth === _this.nowMonth) { //현재 날짜
 					if (day_date.getDate() === selectDate) {
 						className = className + " bg";
 					}
 				}
-
-				//여는 tr 닫는 tr 처리
-				if (nowDay === 0) {
+				if (nowDay === 0) { //tr 여는 태그
 					openTr = '<tr>';
 				}
-				if (nowDay === 6) {
+
+				if (nowDay === 6) { //tr 닫는 태그
+					closeTr = '</tr>';
 					closeTr = '</tr>';
 				}
 
-				//todo날짜 셋팅필요
+				//td 만들기
 				td = openTr + '<td class="' + className + '"><a href="javascript:;" data-date="' + _nowDate + '">' + _this.d + '<span id="'+  _this.d +'"></span></a></td>' + closeTr;
 				_this.d++;
 			}
@@ -140,8 +147,7 @@ class Calendar {
 		monthBox = monthBox + '</tr></table>';
 		document.getElementById(_this.calendarId).innerHTML = monthBox;
 
-
-		if(_this.url) {
+		if(_this.url) { //JSon 호출
 			_this.JsonUrl(_this.url).then(function(responseObject) {
 				for(var a = 0; a < _this.lastDate; a++){
 					var span = document.getElementById(_this.calendarId).getElementsByTagName("span");
@@ -152,9 +158,9 @@ class Calendar {
 							todoDayM = parseInt(todoDay[1]),
 							todoDayD = parseInt(todoDay[2]);
 							if(todoDayY === _this.year && todoDayM === _this.nowMonth && spanId === String(todoDayD)) {
-								/*if(responseObject.TodoList[i].holiyday === "holiyday") {
-									_this.className = _this.className + " holiy";
-								}*/
+								if(responseObject.TodoList[i].holiyday === "holiyday") {
+									span[a].parentNode.parentNode.className += " holiy";
+								}
 								_this.todo = responseObject.TodoList[i].dateTodo;
 								span[a].innerHTML = _this.todo;
 							}
@@ -162,7 +168,6 @@ class Calendar {
 					}
 			});
 		}
-
 
 		//이전달
 		var prev_btn = document.getElementById(_this.calendarId).getElementsByClassName('prev_btn')[0];
@@ -184,27 +189,25 @@ class Calendar {
 				document.getElementById(_this.inputId).className += " active";
 			}
 		});
-
-		//날짜 이벤트
+		//날짜클릭시
 		var tdTag = document.getElementById(_this.calendarId).getElementsByTagName("td");
 		for (var i = 0; i < tdTag.length; i++) {
 			var el = tdTag[i].getElementsByTagName("a");
 			for (var h = 0; h < el.length; h++) {
 				el[h].addEventListener("click", function () {
 					document.getElementById(_this.inputId).setAttribute("value", this.getAttribute('data-date'));
-					if (_this.DateOnClick !== undefined) { //날짜클릭시 이벤트
+					if (_this.DateOnClick !== undefined) {
 						_this.DateOnClick(this.getAttribute('data-date'));
 					}
-					_this.CalendarClickClose(); //달력레이어 닫기
+					_this.CalendarClickClose();//날짜클릭시
 				});
 			}
 		}
-
-		//달력레이어 닫기버튼
-		var a_click = document.getElementById(_this.calendarId).getElementsByClassName('close_btn')[0];
-		a_click.addEventListener("click", function () {
-			_this.CalendarClickClose(); //달력레이어 닫기
-			if (_this.CalendarCloseBtnOnClick !== undefined) { //달력레이어 닫기버튼 클릭시 이벤트
+		//날짜클릭시
+		var close_btn = document.getElementById(_this.calendarId).getElementsByClassName('close_btn')[0];
+		close_btn.addEventListener("click", function () {
+			_this.CalendarClickClose();
+			if (_this.CalendarCloseBtnOnClick !== undefined) {
 				_this.CalendarCloseBtnOnClick(_this.nowDate);
 			}
 		});
@@ -228,7 +231,7 @@ class Calendar {
 		});
 	}
 
-	CalendarClickOpen() { //달력레이어 열기
+	CalendarClickOpen() {
 		var _this = this;
 		document.getElementById(_this.inputId).addEventListener("click", function(){
 			if(this.getAttribute('class') !== "input_date active"){
@@ -242,7 +245,7 @@ class Calendar {
 				if(inputDateDay  != 'Invalid Date' ){
 					this.className += " active";
 					if(document.getElementById(_this.calendarId) === null){
-						_this.create(this.value); //this.value
+						_this.create(this.value);
 					}
 					document.getElementById(_this.calendarId).className += " on";
 
@@ -253,16 +256,16 @@ class Calendar {
 		});
 	}
 
-	CalendarClickClose() { //달력레이어 닫기
+	CalendarClickClose() {
 		var _this = this;
 		document.getElementById(_this.calendarId).remove();
 		document.getElementById(_this.inputId).setAttribute("class","input_date");
 	}
 
-	set OnDateClick(event) { //날짜클릭시 이벤트
+	set OnDateClick(event) {
 		this.DateOnClick = event;
 	}
-	set OnCalendarCloseClick(event){ //달력레이어 닫기버튼 클릭시 이벤트
+	set OnCalendarCloseClick(event){
 		this.CalendarCloseBtnOnClick = event;
 	}
 }
