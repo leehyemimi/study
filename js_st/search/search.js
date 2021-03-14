9b509778a63b523b34b27e82cbe3190f66d50662
leehyemimi <sunone222@gmail.com>
2021-03-14 21:19:33 +0900

    정리

--- js_st/search/search.js
+++ js_st/search/search.js
@@ -12,8 +12,8 @@ class Search {
 	}
 	initEvent(){
 		var _this = this;
-		//li_list
-		for(var i=0 ; i< _this.li.length; i++){
+		//자동완성리스트
+		for(var i=0 ; i<_this.li.length; i++){
 			_this.li[i].addEventListener("click", function () {
 				_this.ActiveCon();
 			});
@@ -26,50 +26,47 @@ class Search {
 			_this.InputActive();
 		});
 	}
-
-	//input에 키보드접근시
-	InputActive(){
-		if(this.ul.className !== "on"){ //레이어열림
+	
+	InputActive(){ //input에 키보드입력시
+		if(this.ul.className !== "on"){ //자동완성리스트 레이어열림
 			this.ul.classList.add('on');
 		}
-		//방향키 위아래
+		//방향키
 		if(event.keyCode === 40){ //아래
 			this.FocusView(this.li[0],this.li[0].innerText);
 		}else if((64 < event.keyCode && event.keyCode < 91)){ //영문 + 국문 // ****국문해야함 모르겠음
 			this.inputTxt = this.input.value;
 		}
-	}
-	//li_list show
-	ActiveCon(){
+	}	
+	ActiveCon(){ //자동완성리스트 show
 		this.input.value = this.innerText;
 		this.ul.classList.remove('on');
-	}
-	//li_list 선택
-	SelectItem(){
-		if(event.keyCode === 40){ //아래
+	}	
+	SelectItem(){ //자동완성리스트 선택
+		if(event.keyCode === 40){ //down
 			if(this.currentIndex < this.li.length-1){
 				this.currentIndex = this.currentIndex + 1;
 				this.FocusView(this.li[this.currentIndex],this.li[this.currentIndex].innerText);
-			}else{
-				this.FocusView(this.input,this.inputTxt);//원래 문자
+			}else{ //마지막
+				this.FocusView(this.input,this.inputTxt); //원래 입력된 문자
 				this.currentIndex = 0;
 			}
-		}else if(event.keyCode === 38) { //위
+		}else if(event.keyCode === 38) { //up
 			if(this.currentIndex > 0){
 				this.currentIndex = this.currentIndex - 1;
 				this.FocusView(this.li[this.currentIndex],this.li[this.currentIndex].innerText);
 			}else{ //첫번째
-				this.FocusView(this.input,this.inputTxt);//원래 문자
+				this.FocusView(this.input,this.inputTxt); //원래 입력된 문자
 			}
-		}else if(event.keyCode === 13){ //엔터
+		}else if(event.keyCode === 13){ //enter
 			this.ul.classList.remove('on');
 			this.FocusView(this.input,this.li[this.currentIndex].innerText);
 			this.currentIndex = 0;
 		}
 	}
 
-	FocusView(focus,inputText){
+	FocusView(focus,inputText){ //포커스 및 input값 변경
 		focus.focus();
-		this.input.value =inputText;
+		this.input.value = inputText;
 	}
 }
